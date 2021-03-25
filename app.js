6d9cc0a61b9766bde5bba01023ad72d7d3125fd7
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-25 19:52:33 +1300

    data types - Primitive & Reference

--- app.js
+++ app.js
@@ -1,70 +1,35 @@
-// Log to console
-console.log('Log to console');
-console.log(321);
-console.log(true);
-var greeting = 'Bad Boy';
-console.log(greeting);
-console.log([1, 2, 3, 4, 5]);
-console.log({ a: 1, b: 2, c: 3 });
-console.table({ a: 1, b: 2, c: 3 });
+// Primitive
 
-console.error('This is an error');
-console.clear();
-console.warn('This is a warning!!');
+// String
+const name = 'John Doe';
+// Number
+const age = 44;
+// Booleon
+const hasKids = true;
+// Null
+const bike = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
 
-/*
-  multi
-  line
-  comments
-*/
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof hasKids);
+console.log(typeof bike);
+console.log(typeof test);
+console.log(typeof sym);
 
-// // Variables
-// var, let, const
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
-
-//Init var
-var greeting;
-console.log(greeting);
-greeting = 'hello';
-console.log(greeting);
-
-// Variables can have letters, numbers, _, $
-// Cannot start with a number
-
-// Multi word variables
-var firstName = 'Cameron'; // Camel case
-var first_name = 'Mason'; // Underscore
-var FirstName = 'Darius'; // Pascal case
-
-// LET
-let name;
-name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
-
-CONST
-const name = 'Liam';
-console.log(name);
-// cannot reassign
-name = 'Cameron';
-// Have to assign a value
-const greeting;
-
-const person = {
-  name: 'Cam',
-  age: 44,
+// Reference Types - Objects
+// Array
+const hobbies = ['waka ama', 'crossfit', 'kapa haka'];
+// Object Literal
+const address = {
+  city: 'Hamilton',
+  suburb: 'Hamilton East',
 };
+const today = new Date();
 
-person.name = 'Pae';
-person.age = 38;
-
-console.log(person);
-
-const num = [1, 2, 3, 4, 5];
-num.push(6, 7);
-
-console.log(num);
+console.log(typeof hobbies);
+console.log(typeof address);
+console.log(typeof today);
