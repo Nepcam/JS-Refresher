63b2d405a24a6115321c7efb1ca4d06cd11042e3
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-25 18:37:08 +1300

    log to console

---
+++ app.js
@@ -0,0 +1,19 @@
+// Log to console
+console.log('Log to console');
+console.log(321);
+console.log(true);
+var greeting = 'Bad Boy';
+console.log(greeting);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2, c: 3 });
+console.table({ a: 1, b: 2, c: 3 });
+
+console.error('This is an error');
+console.clear();
+console.warn('This is a warning!!');
+
+/*
+  multi
+  line
+  comments
+*/
