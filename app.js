649903970118688b20230d755de3163cd6e9bdad
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-25 19:04:01 +1300

    variables

--- app.js
+++ app.js
@@ -17,3 +17,54 @@ console.warn('This is a warning!!');
   line
   comments
 */
+
+// // Variables
+// var, let, const
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+//Init var
+var greeting;
+console.log(greeting);
+greeting = 'hello';
+console.log(greeting);
+
+// Variables can have letters, numbers, _, $
+// Cannot start with a number
+
+// Multi word variables
+var firstName = 'Cameron'; // Camel case
+var first_name = 'Mason'; // Underscore
+var FirstName = 'Darius'; // Pascal case
+
+// LET
+let name;
+name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+CONST
+const name = 'Liam';
+console.log(name);
+// cannot reassign
+name = 'Cameron';
+// Have to assign a value
+const greeting;
+
+const person = {
+  name: 'Cam',
+  age: 44,
+};
+
+person.name = 'Pae';
+person.age = 38;
+
+console.log(person);
+
+const num = [1, 2, 3, 4, 5];
+num.push(6, 7);
+
+console.log(num);
