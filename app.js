c62815ade567cb599e8079fc085384fe01cd124c
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-26 14:05:23 +1300

    String Methods and Concatenation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const firstName = 'Cameron';
 const lastName = 'Nepe';
 const age = 44;
+const str = 'Kia ora my name is Cameron';
+const tags = 'web design, web dev, web prog';
 
 let val;
 
@@ -14,6 +16,47 @@ val = 'John ';
 val += 'Doe';
 
 val = 'Kia ora, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = "That's awesome, we're off!!";
+
+// Length
+val = firstName.length;
+
+// Concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[3];
+
+// indexOf()
+val = lastName.indexOf('e');
+val = lastName.lastIndexOf('e');
+
+// charAt()
+val = firstName.charAt('2');
+// Get last character
+val = firstName.charAt(firstName.length - 1);
+
+// Substring()
+val = firstName.substring(0, 4);
+
+// Slice
+val = firstName.slice(0, 3);
+val = firstName.slice(-3);
+
+// Split
+val = str.split(' ');
+val = tags.split(',');
+
+// Replace()
+val = str.replace('Cameron', 'Liam');
+
+// Includes()
+val = str.includes('Kia ora');
 
 // Output
 console.log(val);
